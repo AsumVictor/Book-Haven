c21a7ae4adc4bef09218fc5b382c53aba129b148
AsumVictor <iamasum369@gmail.com>
2023-07-21 06:10:05 -0700

    Added setup

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      
+      </Routes>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </BrowserRouter>
+  );
+}
+
+export default App;
