6ab8a5fab71166bca41c7fed3bfb94e5bb5e3058
AsumVictor <iamasum369@gmail.com>
2023-07-27 20:55:49 +0000

    Updated readme file

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,18 +1,14 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import Lottie from 'lottie-react';
-import { removeBook } from '../redux/book/bookSlice';
-import ProgressBar from './Progressbar';
-import deleteAnim from '../assets/animation_json/deleting.json';
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import Lottie from "lottie-react";
+import { removeBook } from "../redux/book/bookSlice";
+import ProgressBar from "./Progressbar";
+import deleteAnim from "../assets/animation_json/deleting.json";
 
-const Book = ({
-  id, title, author, currentChapter, progress,
-}) => {
+const Book = ({ id, title, author, currentChapter, progress }) => {
   const dispatch = useDispatch();
-  const { deletedSuccess } = useSelector(
-    (state) => state.book,
-  );
+  const { deletedSuccess } = useSelector((state) => state.book);
 
   const [click, setClick] = useState(false);
   const handleRemove = () => {
@@ -23,12 +19,12 @@ const Book = ({
   return (
     <div
       className={`book ${
-        click && deletedSuccess && 'deleted'
+        click && deletedSuccess && "deleted"
       } w-full bg-white relative hover:border-_blue border rouned-md grid 730px:grid-cols-2 gap-y-5 pb-3`}
     >
       <div
         className={`${
-          click ? 'opacity-20' : ''
+          click ? "opacity-20" : ""
         }  px-2 md:px-6 pt-4 py-2 flex flex-col gap-3`}
       >
         <h3 className="font-bold text-gray-500">Category</h3>
@@ -40,7 +36,9 @@ const Book = ({
         </p>
         <div className="w-10/12 py-2 850px:w-10/12 1059px:w-7/12 grid grid-cols-3 mt-5">
           <div className="h-[1cm] flex items-center justify-start col-span-1 border-r-2 border-gray-200">
-            <button type='button' className="text-_blue text-[18px]">Comment</button>
+            <button type="button" className="text-_blue text-[18px]">
+              Comment
+            </button>
           </div>
           <div className="h-[1cm] flex items-center justify-center col-span-1 border-r-2 border-gray-200">
             <button
@@ -52,11 +50,13 @@ const Book = ({
             </button>
           </div>
           <div className="h-[1cm] flex items-center justify-center col-span-1 border-r-2 border-gray-200">
-            <button  type='button' className="text-_blue text-[18px]">Edit</button>
+            <button type="button" className="text-_blue text-[18px]">
+              Edit
+            </button>
           </div>
         </div>
       </div>
-      <div className={`${click ? 'opacity-20' : ''}   grid grid-cols-2`}>
+      <div className={`${click ? "opacity-20" : ""}   grid grid-cols-2`}>
         <div className=" col-span-1 flex items-center justify-center">
           <div className="w-full h-[3.5cm] flex items-center justify-center 850px:gap-2  border-r-2 border-r-gray-300 pr-2 gap-0">
             <ProgressBar
@@ -65,8 +65,7 @@ const Book = ({
             />
             <h3 className="flex flex-col text-center max-730px:-ml-4">
               <span className="font-bold text-xl 850px:text-2xl 1060px:text-3xl">
-                {Math.ceil(progress * 100)}
-                %
+                {Math.ceil(progress * 100)}%
               </span>
               <span className="text-gray-500 semi-semibold 850px:text-[16px] 1060px:text-xl">
                 Completed
@@ -79,9 +78,7 @@ const Book = ({
             CURRENT CHAPTER
           </h3>
           <h3 className="text-[18px] font-semibold text-black">
-            Chapter
-            {' '}
-            {currentChapter}
+            Chapter {currentChapter}
           </h3>
           <button
             type="button"
@@ -111,8 +108,8 @@ Book.propTypes = {
 };
 
 Book.defaultProps = {
-  title: 'Book title',
-  author: 'Unknown Author',
+  title: "Book title",
+  author: "Unknown Author",
   progress: 0,
   currentChapter: 1,
 };
